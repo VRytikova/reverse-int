ac63cf5319df1b60c99c50b6edeb4a5c4dc8ab73
RVeronica <akinrv16@gmail.com>
2021-03-15 00:31:25 +0300

    implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    if(n>0) {
+        let reverseInt = String(n);
+        let arrayInt = reverseInt.split('');
+        let newArrayInt = arrayInt.reverse();
+        return Number(newArrayInt.join(''));
+    } else{
+        let reverseInt = String(-n);
+        let arrayInt = reverseInt.split('');
+        let newArrayInt = arrayInt.reverse();
+        return Number(newArrayInt.join(''));
+    }
 }
